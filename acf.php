6c9cb3128dc0ac1c3efb3ff812e73db6987510b8
elliotcondon <elliotcondon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-10 05:14:34 +0000

    Fixed issue with parse_types function not applying to some array pieces
    git-svn-id: http://plugins.svn.wordpress.org/advanced-custom-fields/trunk@694987 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- acf.php
+++ acf.php
@@ -10,7 +10,7 @@ License: GPL
 Copyright: Elliot Condon
 */
 
- 
+
 // lite mode
 if( !defined('ACF_LITE') )
 {
@@ -210,7 +210,7 @@ class Acf
 			foreach( $value as $k => $v )
 			{
 				// if $field was passed, never modify the value! NEVER!
-				if( $k == 'value' )
+				if( $k === 'value' )
 				{
 					continue;
 				}
